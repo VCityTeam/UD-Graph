9e2ded7264db7cf549972e1c16433e270a91e85a
Diego Vinasco <diego.vinasco@protonmail.com>
2021-01-21 19:50:41 +0100

    fix entrypoint bug - remove shebang line - add default args to uml2owl argument parser - update uml2owl argument naming conventions

--- Transformations/entrypoint.py
+++ Transformations/entrypoint.py
@@ -1,4 +1,3 @@
-#!usr/bin/python
 import os
 import argparse
 
@@ -8,8 +7,8 @@ def main():
     subparsers = parser.add_subparsers(dest='transformation', help='select transformation pipeline')
     uml2owl_parser = subparsers.add_parser('uml2owl', help='transform a uml model to an owl ontology via shapechange')
     uml2owl_parser.add_argument('--config', required=True, help='required, specify the shapechange configuration file')
-    uml2owl_parser.add_argument('--model', help='specify the uml file to transform')
-    uml2owl_parser.add_argument('--output', help='specify the output directory')
+    uml2owl_parser.add_argument('--input', default='', help='specify the uml file to transform')
+    uml2owl_parser.add_argument('--output', default='', help='specify the output directory')
     xml2rdf_parser = subparsers.add_parser('xml2rdf', help='transform an xml file into a rdf graph')
     xml2rdf_parser.add_argument('--input', required=True, help='specify the input file to transform')
     xml2rdf_parser.add_argument('--model', required=True, help='specify the ontology to base the transformation on')
@@ -21,7 +20,7 @@ def main():
 
     command = ''
     if args.transformation == 'uml2owl':
-        command = f"java -jar lib/ShapeChange-2.9.1.jar -Dfile.encoding=UTF-8 -c {args.input} -x '$input$' '{args.model}' -x '$output$' '{args.output}'"
+        command = f"java -jar lib/ShapeChange-2.9.1.jar -Dfile.encoding=UTF-8 -c {args.config} -x '$input$' '{args.input}' -x '$output$' '{args.output}'"
     elif args.transformation == 'xml2rdf':
         command = f'python XML-to-RDF/OWL-based-transformations/CityGML2RDF.py {args.input} {args.model} {args.output}'
     elif args.transformation == 'xsd2owl':
