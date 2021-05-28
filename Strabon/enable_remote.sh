cd3e847c495897c3ccfdee4e2c5739387d34ad96
DiegoVinasco <diego.vinasco@protonmail.com>
2021-05-28 09:58:20 +0200

    removed strabon

--- Strabon/enable_remote.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo -e "host \t all \t all \t 0.0.0.0/0 \t md5" >> /etc/postgresql/9.1/main/pg_hba.conf
-echo "listen_addresses='*'" >> /etc/postgresql/9.1/main/postgresql.conf
-cat /etc/postgresql/9.1/main/pg_hba.conf | grep host
-cat /etc/postgresql/9.1/main/postgresql.conf | grep listen_addresses
--- Strabon/rocket.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec /tomcat/bin/catalina.sh run
