c9a39f9bc095de3de04bedcf7cf2b63953cc1080
Diego Vinasco <diego.vinasco@protonmail.com>
2021-07-12 17:08:54 +0200

    add initial sparql module code skeleton

--- UD-Graph-App/src/SparqlModule/SparqlModule.js
+++ UD-Graph-App/src/SparqlModule/SparqlModule.js
@@ -1,11 +1,42 @@
 import './SparqlModule.css';
 
-import { ModuleView } from 'ud-viz'
+import { ModuleView, Window } from 'ud-viz'
 
 export class SparqlModuleView extends ModuleView {
     constructor() {
         super()
+    
+        /**
+         * The SPARQL query window.
+         *
+         * @type {SparqlQueryWindow}
+         */
+        this.sparqlQueryWindow = new SparqlQueryWindow();
+    
+        /**
+         * The different windows of the view.
+         *
+         * @type {Array<Window>}
+         */
+        this.windows = [];
+    
+        this.addWindow(this.sparqlQueryWindow);
+    
+        this.navigatorWindow.addEventListener(Window.EVENT_DISABLED, () => {
+          this.disable();
+        });
+
     }
 
-    // TODO: implement SPARQL MODULE code
+    // TODO: implement addWindow() method
+
+    // Display the view
+    enableView() {
+        this.appendToElement(this.parentElement); //calls a method that creates the DOM elements
+    }
+
+    // Close the view
+    disableView() {
+        this.dispose(); //This methods destroys the DOM elements
+    }
 }
