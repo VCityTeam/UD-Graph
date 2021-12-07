c5108db9f00b1a39700b22f355d03d27deeadf13
Diego Vinasco <diego.vinasco@protonmail.com>
2021-12-07 11:41:04 +0100

    add custom versioning uri functionality and minor bug fixes

--- Transformations/DifferenceGraph-to-CityGML3/DifferenceGraph2RDF.py
+++ Transformations/DifferenceGraph-to-CityGML3/DifferenceGraph2RDF.py
@@ -8,14 +8,33 @@ from rdflib.namespace import XSD, RDF, RDFS, OWL, Namespace
 def main():
     # initialize command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', help='specify the input CityGML datafile')
-    parser.add_argument('time_stamps', nargs=2, help='specify the input timestamps')
-    parser.add_argument('--output_dir', default='.', help='specify the output directory')
-    parser.add_argument('--uri', default='https://github.com/VCityTeam/UD-Graph/data#', help='specify the base URI for output individuals')
-    parser.add_argument('--prefix', default='data', help='specify the base URI prefix for output individuals')
-    parser.add_argument('-f', '--format', default='ttl', choices=['ttl', 'xml'], help='specify the output RDF format [xml, ttl]')
-    parser.add_argument('-l', '--log', default='output.log', help='specify the logging file')
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug level logging')
+    parser.add_argument('input_file',
+                         help='specify the input CityGML datafile')
+    parser.add_argument('time_stamp',
+                         nargs=2,
+                         help='specify the input timestamps')
+    parser.add_argument('--output_dir',
+                         default='.',
+                         help='specify the output directory')
+    parser.add_argument('--base-uri',
+                         default='https://github.com/VCityTeam/UD-Graph/data#',
+                         help='specify the base URI for output individuals')
+    parser.add_argument('--prefix',
+                         default='data',
+                         help='specify the base URI prefix for output individuals')
+    parser.add_argument('--versioning-uri',
+                         default='https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/CityGML/3.0/versioning#',
+                         help='specify the URI for the CityGML 3.0 versioning module')
+    parser.add_argument('-f', '--format',
+                         default='ttl',
+                         choices=['ttl', 'xml'],
+                         help='specify the output RDF format')
+    parser.add_argument('-l', '--log',
+                         default='output.log',
+                         help='specify the logging file')
+    parser.add_argument('-d', '--debug',
+                         action='store_true',
+                         help='enable debug level logging')
     args = parser.parse_args()
 
     # initialize logging
@@ -28,6 +47,8 @@ def main():
                             level=logging.WARNING,
                             format='%(asctime)s %(levelname)-8s %(message)s')
 
+    logging.debug(f'received arguments: {args}')
+
     # read input file
     logging.info(f'loading input file: {args.input_file}...')
     with open(args.input_file, "r") as file:
@@ -35,16 +56,16 @@ def main():
 
     # create output version graph and set namespaces
     output_graph = Graph()
-    VERS = Namespace('https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/CityGML/3.0/versioning#')
+    VERS = Namespace(args.versioning_uri)
     output_graph.namespace_manager.bind( 'owl', OWL )
     output_graph.namespace_manager.bind( 'vers', VERS )
-    output_graph.namespace_manager.bind( args.prefix, URIRef(args.uri) )
+    output_graph.namespace_manager.bind( args.prefix, URIRef(args.base_uri) )
 
 
     # add VersionTransition and Version
-    source_version_uri = URIRef( f'{args.uri}version_{args.time_stamps[0]}' )
-    target_version_uri = URIRef( f'{args.uri}version_{args.time_stamps[1]}' )
-    versiontransaction_uri = URIRef( f'{args.uri}_versiontransition_{args.time_stamps[0]}_{args.time_stamps[1]}' )
+    source_version_uri = URIRef( f'{args.base_uri}version_{args.time_stamp[0]}' )
+    target_version_uri = URIRef( f'{args.base_uri}version_{args.time_stamp[1]}' )
+    versiontransaction_uri = URIRef( f'{args.base_uri}versiontransition_{args.time_stamp[0]}_{args.time_stamp[1]}' )
 
     output_graph.add( (source_version_uri, RDF.type, VERS.Version) )
     output_graph.add( (source_version_uri, RDF.type, OWL.NamedIndividual) )
@@ -52,32 +73,32 @@ def main():
     output_graph.add( (target_version_uri, RDF.type, OWL.NamedIndividual) )
     output_graph.add( (versiontransaction_uri, RDF.type, VERS.VersionTransition) )
     output_graph.add( (versiontransaction_uri, RDF.type, OWL.NamedIndividual) )
+    # VERS.from causes a syntax error
+    # output_graph.add( (versiontransaction_uri, VERS.from, source_version_uri) )
     from_uri = URIRef( VERS + 'from' )
     output_graph.add( (versiontransaction_uri, from_uri, source_version_uri) )
-    # VERS.from causes a syntax error since the 'from' function is reserved for imports 
-    # output_graph.add( (versiontransaction_uri, VERS.from, source_version_uri) )
     output_graph.add( (versiontransaction_uri, VERS.to, target_version_uri) )
 
     # populate versions' versionMembers
     for node in input_graph.get('nodes'):
         node_gid = node.get("globalid")
-        node_uri = URIRef( args.uri + node_gid )
-        if node_gid.startswith(args.time_stamps[0]):
+        node_uri = URIRef( args.base_uri + node_gid )
+        if node_gid.startswith(args.time_stamp[0]):
             output_graph.add( (source_version_uri, VERS.versionMember, node_uri) )
-        elif node_gid.startswith(args.time_stamps[1]):
+        elif node_gid.startswith(args.time_stamp[1]):
             output_graph.add( (target_version_uri, VERS.versionMember, node_uri) )
         else:
             logging.error(f'could not determine parent version of node: {node_gid}')
     
     # populate versionTransition's Transactions
     for edge in input_graph.get('edges'):
-        transaction_uri = URIRef( f'{args.uri}transaction_{edge.get("id")}' )
+        transaction_uri = URIRef( f'{args.base_uri}transaction_{edge.get("id")}' )
         transaction_type = Literal( edge.get('type'), datatype=XSD.string )
         transaction_tags = Literal( edge.get('tags'), datatype=XSD.string )
         source_node = getNodeById(edge.get('source'), input_graph.get('nodes'))
-        source_node_uri = URIRef( args.uri + source_node.get('globalid') )
+        source_node_uri = URIRef( args.base_uri + source_node.get('globalid') )
         target_node = getNodeById(edge.get('target'), input_graph.get('nodes'))
-        target_node_uri = URIRef( args.uri + target_node.get('globalid') )
+        target_node_uri = URIRef( args.base_uri + target_node.get('globalid') )
 
         output_graph.add( (transaction_uri, RDF.type, VERS.Transaction) )
         output_graph.add( (transaction_uri, RDF.type, OWL.NamedIndividual) )
--- Transformations/DifferenceGraph-to-CityGML3/readme.md
+++ Transformations/DifferenceGraph-to-CityGML3/readme.md
@@ -6,20 +6,23 @@ The difference graph is created by the stage 4 output of the [cityGMLto3DTiles](
 
 ## To run
 ```
-usage: DifferenceGraph2RDF.py [-h] [--output_dir OUTPUT_DIR] [--uri URI] [--prefix PREFIX] [-f {ttl,xml}] [-l LOG] [-d] input_file time_stamps time_stamps
+usage: DifferenceGraph2RDF.py [-h] [--output_dir OUTPUT_DIR] [--base-uri BASE_URI] [--prefix PREFIX] [--versioning-uri VERSIONING_URI] [-f {ttl,xml}] [-l LOG] [-d]
+                              input_file time_stamp time_stamp
 
 positional arguments:
   input_file            specify the input CityGML datafile
-  time_stamps           specify the input timestamps
+  time_stamp            specify the input timestamps
 
 optional arguments:
   -h, --help            show this help message and exit
   --output_dir OUTPUT_DIR
                         specify the output directory
-  --uri URI             specify the base URI for output individuals
+  --base-uri BASE_URI   specify the base URI for output individuals
   --prefix PREFIX       specify the base URI prefix for output individuals
+  --versioning-uri VERSIONING_URI
+                        specify the URI for the CityGML 3.0 versioning module
   -f {ttl,xml}, --format {ttl,xml}
-                        specify the output RDF format [xml, ttl]
+                        specify the output RDF format
   -l LOG, --log LOG     specify the logging file
   -d, --debug           enable debug level logging
 ```
