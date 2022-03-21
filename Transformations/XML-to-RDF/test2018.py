68889b19dcfe97bd0d17f794d11cc7326e8931ca
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2022-03-21 10:35:59 +0100

    CityGML2RDF refacto - add  flags to cusomize gmlliteral generation - use an OO class and functions to run transform instead of global variables

---
+++ Transformations/XML-to-RDF/test2018.py
@@ -0,0 +1,37 @@
+import os
+
+### transform CityGML 2 ###
+
+command = (r'python ./CityGML2RDF.py '
+    r'-v '
+    r'--format rdf '
+    r'--log output2018v2.log '
+    r'..\..\..\citygml2-to-citygml3\input\GratteCiel_2018_split.gml '
+    r'..\..\Ontologies\CityGML\2.0,'
+    # r'../test-data/OWL/compositeCityGML2.0.rdf,'
+    # r'../test-data/OWL/TC_211,'
+    # r'../test-data/OWL/GeoSPARQL '
+    r'https://www.w3.org/2009/08/skos-reference/skos.rdf,'
+    r'http://www.opengis.net/ont/geosparql#,'
+    r'http://www.opengis.net/ont/gml# '
+    r'./citygml_2_mappings.json')
+
+print(command)
+os.system(command)
+
+command = (r'python CityGML2RDF.py '
+    r'-v '
+    r'--format rdf '
+    r'--log output2018v3.log '
+    r'..\..\..\citygml2-to-citygml3\output\GratteCiel_2018_split_v3.gml '
+    r'..\..\Ontologies\CityGML\3.0,'
+    # r'../test-data/OWL/compositeCityGML3.0.rdf,'
+    # r'../test-data/OWL/TC_211,'
+    # r'../test-data/OWL/GeoSPARQL '
+    r'https://www.w3.org/2009/08/skos-reference/skos.rdf,'
+    r'http://www.opengis.net/ont/geosparql#,'
+    r'http://www.opengis.net/ont/gml# '
+    r'citygml_3_mappings.json')
+
+print(command)
+os.system(command)
