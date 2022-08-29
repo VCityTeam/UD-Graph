3de64c72e284f1e18897651548ad088b5f7f02dd
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2022-08-29 18:16:27 +0200

    add document .owl ontology and update swrl_rules.py

--- rules/swrl_rules.py
+++ rules/swrl_rules.py
@@ -1,21 +1,47 @@
 from owlready2 import *
 
+def load_ontology(file):
+    ontology = get_ontology(file).load()
+    print(ontology)
+    return ontology
 
-def load_ontology():
-    onto = get_ontology("file://../Ontologies/Workspace/3.0/workspace.owl").load()
-    return (onto)
+def load_ontologies(ontology_list):
+    ontology_network = get_ontology(ontology_list[0]).load()
+    print(ontology_network)
+    for ontology_location in ontology_list[1:]:
+        ontology = get_ontology(ontology_location).load()
+        print(ontology)
+        ontology_network.imported_ontologies.append(ontology)
+    return ontology_network
 
-def get_classes(onto):
-    for i in onto.classes():
-        print(i.iri)
+def get_classes(ontology):
+    for _class in ontology.classes():
+        print(_class.iri)
 
-def add_rules(onto):
-    ns = onto.get_namespace("https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/Workspace/3.0/workspace")
-    rule = Imp(namespace=ns)
-    rule.set_as_rule("""ConcensusSpace(?c), Space.scenarioMember(?c, ?s) -> Scenario(?s)""")
-    print(str(rule))
+def add_rules(ontology, namespace_list, rule_file):
+    ns = ontology.get_namespace(namespace_list[0])
+    print(ns)
+    with open(rule_file, 'r') as rules:
+        rule = Imp(namespace=ns)
+        for line in rules:
+            rule.set_as_rule(line, namespaces=[ns])
+            print(str(rule))
 
 
-onto = load_ontology()
-get_classes(onto)
-add_rules(onto)
+
+ontology_list = [
+    'file://../Ontologies/Workspace/3.0/workspace.owl',
+    'file://../Ontologies/CityGML/3.0/core.owl',
+    'file://../Ontologies/CityGML/3.0/versioning.owl',
+    'file://../Ontologies/Document/3.0/document.owl'
+]
+namespace_list = [
+    'https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/Workspace/3.0/workspace',
+    'https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/CityGML/3.0/core',
+    'https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/CityGML/3.0/versioning',
+    'https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Ontologies/Document/3.0/document'
+]
+
+ontology_network = load_ontologies(ontology_list)
+get_classes(ontology_network)
+add_rules(ontology_network, namespace_list, 'workspace_rules.txt')
