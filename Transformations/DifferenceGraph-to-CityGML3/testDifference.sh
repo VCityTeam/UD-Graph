a540f1853d77539993058817ce2784e73e3f6a6d
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2022-10-12 14:09:34 +0200

    fix timestamp propery generation and update gratte ciel workspace dataset

--- Transformations/DifferenceGraph-to-CityGML3/testDifference.sh
+++ Transformations/DifferenceGraph-to-CityGML3/testDifference.sh
@@ -3,9 +3,9 @@
 python DifferenceGraph2RDF.py \
     --base-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009-2018_Workspace \
     --v1-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009_split_v3 \
-    --v1-prefix 2009 \
+    --v1-prefix v2009 \
     --v2-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2012_split_v3 \
-    --v2-prefix 2012 \
+    --v2-prefix v2012 \
     -d \
     -f ttl \
     --strip-time-stamp \
@@ -19,9 +19,9 @@ python DifferenceGraph2RDF.py \
 python DifferenceGraph2RDF.py \
     --base-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009-2018_Workspace \
     --v1-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2012_split_v3 \
-    --v1-prefix 2012 \
+    --v1-prefix v2012 \
     --v2-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2015_split_v3 \
-    --v2-prefix 2015 \
+    --v2-prefix v2015 \
     -d \
     -f ttl \
     --strip-time-stamp \
@@ -35,9 +35,9 @@ python DifferenceGraph2RDF.py \
 python DifferenceGraph2RDF.py \
     --base-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009-2018_Workspace \
     --v1-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2015_split_v3 \
-    --v1-prefix 2015 \
+    --v1-prefix v2015 \
     --v2-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2018_split_v3 \
-    --v2-prefix 2018 \
+    --v2-prefix v2018 \
     -d \
     -f ttl \
     --strip-time-stamp \
@@ -47,51 +47,3 @@ python DifferenceGraph2RDF.py \
     'DifferencesAsGraph15-18.json' \
     2015 2018 \
     'DifferencesAsGraph2015-2018.ttl'
-
-python DifferenceGraph2RDF.py \
-    --base-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009-2018_Workspace \
-    --v1-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009_split_v3 \
-    --v1-prefix 2009 \
-    --v2-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2012_split_v3 \
-    --v2-prefix 2012 \
-    -d \
-    -f xml \
-    --strip-time-stamp \
-    --existence_time_stamps \
-        2009-01-01T00:00:00 \
-        2012-01-01T00:00:00 \
-    'DifferencesAsGraph09-12.json' \
-    2009 2012 \
-    'DifferencesAsGraph2009-2012.rdf'
-
-python DifferenceGraph2RDF.py \
-    --base-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009-2018_Workspace \
-    --v1-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2012_split_v3 \
-    --v1-prefix 2012 \
-    --v2-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2015_split_v3 \
-    --v2-prefix 2015 \
-    -d \
-    -f xml \
-    --strip-time-stamp \
-    --existence_time_stamps \
-        2012-01-01T00:00:00 \
-        2015-01-01T00:00:00 \
-    'DifferencesAsGraph12-15.json' \
-    2012 2015 \
-    'DifferencesAsGraph2012-2015.rdf'
-
-python DifferenceGraph2RDF.py \
-    --base-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2009-2018_Workspace \
-    --v1-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2015_split_v3 \
-    --v1-prefix 2015 \
-    --v2-uri https://raw.githubusercontent.com/VCityTeam/UD-Graph/master/Datasets/GratteCiel_2018_split_v3 \
-    --v2-prefix 2018 \
-    -d \
-    -f xml \
-    --strip-time-stamp \
-    --existence_time_stamps \
-        2015-01-01T00:00:00 \
-        2018-01-01T00:00:00 \
-    'DifferencesAsGraph15-18.json' \
-    2015 2018 \
-    'DifferencesAsGraph2015-2018.rdf'
