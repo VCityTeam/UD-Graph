fc06e4ca64748067a9814c99e3e8c358ae7aaea7
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2022-11-24 16:16:35 +0100

    add docstrings

--- rules/swrl_rules.py
+++ rules/swrl_rules.py
@@ -5,6 +5,16 @@ from rdflib import Graph, Namespace
 
 
 def load_ontologies(ontology_list, world):
+    """
+    Load the ontologies in the list `ontology_list` into the `world`. Additionally,
+    an `ontology network` is created by importing all of the ontologies in the list
+    into the first ontology in the list through imports. Return the ontology
+    network
+    
+    :param ontology_list: a list of ontology locations
+    :param world: The world object that contains the ontology
+    :return: The ontology network is being returned.
+    """
     ontology_network = world.get_ontology(ontology_list[0]).load()
     logging.debug(ontology_network)
     for ontology_location in ontology_list[1:]:
@@ -14,6 +24,11 @@ def load_ontologies(ontology_list, world):
     return ontology_network
 
 def log_rules(ontology):
+    """
+    Log all the rules in an ontology and its imported ontologies
+    
+    :param ontology: the ontology to be used
+    """
     logging.debug('rules:')
     for rule in ontology.rules():
         logging.debug(f'  {rule}')
@@ -23,6 +38,11 @@ def log_rules(ontology):
                 logging.debug(f'  {rule}')
 
 def log_individuals(ontology):
+    """
+    Log the IRI of each individual in the ontology and its imported ontologies
+    
+    :param ontology: the ontology to be logged
+    """
     logging.debug('individuals:')
     for individual in ontology.individuals():
         logging.debug(f'  {individual.iri}')
@@ -32,6 +52,11 @@ def log_individuals(ontology):
                 logging.debug(f'  {individual.iri}')
 
 def log_classes(ontology):
+    """
+    Log the IRI of every class in the ontology and its imported ontologies
+    
+    :param ontology: the ontology to be processed
+    """
     logging.debug('classes:')
     for _class in ontology.classes():
         logging.debug(f'  {_class.iri}')
@@ -41,21 +66,36 @@ def log_classes(ontology):
                 logging.debug(f'  {_class.iri}')
 
 def get_classes(ontology):
+    """
+    Return a list of all classes in an ontology and its imported ontologies
+    
+    :param ontology: The ontology to be used
+    :return: A list of classes in the ontology.
+    """
     classes = [_class for _class in ontology.classes()]
     if type(ontology) is Ontology:
         classes += [_class for _class in ontology.imported_ontologies]
     return classes
 
 def add_rules(ontology, config):
-    rules = format_rules(config)
-    ns = ontology.get_namespace('http://www.w3.org/2000/01/rdf-schema#')
-    print(ns)
-    for rule in rules:
-        logging.info(f'loading rule: {rule}')
-        implication = Imp(namespace=ns)
-        implication.set_as_rule(rule)
-
-def format_rules(config):
+    """
+    It takes a list of rules and adds them to an ontology
+    
+    :param ontology: the ontology object
+    :param config: a dictionary of the form:
+        {
+            "ontologies": [], # a list of ontology location strings in the forms "file://..." or "http://..."
+            "prefixes": {
+                "string": "string",   # where the prefix and namespace uri could be for example: "owl": "http://www.w3.org/2002/07/owl#"; these prefixes can be used in the defined rules
+            },
+            "rules": [  # a list of objects for configuring rules formed as:
+                {
+                    "ignore": "boolean",
+                    "rule": "atom1(?a), atom2(?b) -> atom3(?a, ?b)"     # prefixes declared in the "prefixes" object will be replaced with their namespace uri
+                },
+            ]
+        }
+    """
     prefixes = config.get('prefixes')
     rules = []
     for rule in config.get('rules'):
@@ -66,9 +106,33 @@ def format_rules(config):
                 'rule': rule.get('rule').replace(prefix + ':', prefixes.get(prefix))
             })
         rules.append(rule.get('rule'))
-    return rules
+    ns = ontology.get_namespace('http://www.w3.org/2000/01/rdf-schema#')
+    for rule in rules:
+        logging.info(f'loading rule: {rule}')
+        implication = Imp(namespace=ns)
+        implication.set_as_rule(rule)
 
 def export_graph(world, test, rules, output_format='ttl'):
+    """
+    Take the world, the test, the rules, and the output format, and exports RDF triples 
+    THAT START WITH A PREDEFINED NAMESPACE to a file
+    
+    :param world: the world object that contains the graph
+    :param test: list in the form:
+        [       # a list of objects for configuring tests formed as:
+            {
+                "ignore": "boolean",
+                "ontologies": [], # a list of ontology location strings in the forms "file://..." or "http://..."
+                "output": {
+                    "namespace": "string",          # the namespace uri to be used when exporting the results of inferencing after a test 
+                    "filename": "string",           # the name of the output file to be exported to
+                    "expected-result": "boolean"    # the expected result of the test, true meaning passed and false meaning failed
+                }
+            },
+        ]
+    :param rules: the ruleset to use
+    :param output_format: (optional) the format of the output file. Defaults to 'ttl' (Turtle); format must be RDFlib compliant
+    """
     graph = world.as_rdflib_graph()
     output = Graph()
     output.bind( '', Namespace(test.get('output').get('namespace')) )
@@ -90,11 +154,17 @@ def export_graph(world, test, rules, output_format='ttl'):
     logging.info(f'graph exported to {output_file}')
 
 
+
+##############################
+### Main test suite script ###
+##############################
+
+
+### init logging
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     filename='output.log',
                     # level=logging.INFO)
                     level=logging.DEBUG)
-
 
 ### init configuration
 rule_config_file = 'rules.json'
