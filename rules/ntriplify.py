9760fe4ef082d3a6d2c06850d717078795c5824f
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2022-11-24 16:25:54 +0100

    add file specification to ntriplify script

--- rules/ntriplify.py
+++ rules/ntriplify.py
@@ -1,11 +1,19 @@
-### A simple script for converting all Turtle RDF files in the current directory to the N-triples RDF format
-
 import os
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description='A simple script for converting all Turtle RDF files in the current directory to the N-triples RDF format')
+    parser.add_argument('--list', '-l', nargs='*', help='specify files to convert')
+    args = parser.parse_args()
+
     path_to_reformat_graph = '../Transformations/utilities/reformat_graph.py'
-    for file in [file for file in os.listdir(os.getcwd()) if file.endswith('.ttl')]:
-        os.system(f'python {path_to_reformat_graph} {file} ttl {file.replace(".ttl", ".nt")} nt')
+
+    if args.list is not None:
+        for file in args.list:
+            os.system(f'python {path_to_reformat_graph} {file} ttl {file.replace(".ttl", ".nt")} nt')
+    else:
+        for file in [file for file in os.listdir(os.getcwd()) if file.endswith('.ttl')]:
+            os.system(f'python {path_to_reformat_graph} {file} ttl {file.replace(".ttl", ".nt")} nt')
 
 if __name__ == "__main__":
     main()
